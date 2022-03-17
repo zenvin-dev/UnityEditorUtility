a5725db8513d948e8a1a4a6817cb7782414d1750
Zenvin <zenvin@outlook.de>
2022-03-17 23:44:01 +0100

    - Add Folders - Add Assembly Definitions - Add ArrayRange attribute and property drawer

---
+++ Editor/ArrayRangePropertyDrawer.cs
@@ -0,0 +1,51 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Zenvin.EditorUtility {
+	[CustomPropertyDrawer (typeof (ArrayRangeAttribute))]
+	public class ArrayRangePropertyDrawer : PropertyDrawer {
+
+		public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
+			// draw original property if it's not an int
+			if (property.propertyType != SerializedPropertyType.Integer) {
+				base.OnGUI (position, property, label);
+				return;
+			}
+
+			// get attribute
+			ArrayRangeAttribute attr = attribute as ArrayRangeAttribute;
+			if (attr == null) {
+				base.OnGUI (position, property, label);
+				return;
+			}
+
+			// get path to array property
+			string[] pathParts = property.propertyPath.Split ('/');
+			string path = "";
+			for (int i = 0; i < pathParts.Length - 1; i++) {
+				path = path + pathParts[i];
+			}
+			path = path + "/" + attr.ArrayProperty;
+			path = path.Trim ('/');
+
+			// find array property
+			SerializedProperty arrProp = property.serializedObject.FindProperty (path);
+
+			// draw original property if array property wasn't found
+			if (arrProp == null) {
+				base.OnGUI (position, property, label);
+				return;
+			}
+
+			int min = 0;
+			int max = arrProp.arraySize - 1;
+			int val = EditorGUI.IntSlider (position, label, property.intValue, Mathf.Min (min, max), Mathf.Max (min, max));
+			property.intValue = val;
+		}
+
+		public override float GetPropertyHeight (SerializedProperty property, GUIContent label) {
+			return base.GetPropertyHeight (property, label);
+		}
+
+	}
+}
---
+++ Runtime/ArrayRangeAttribute.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Zenvin.EditorUtility {
+	public class ArrayRangeAttribute : PropertyAttribute {
+
+		public readonly string ArrayProperty;
+
+		public ArrayRangeAttribute (string arrayProperty) {
+			ArrayProperty = arrayProperty;
+		}
+	}
+}
