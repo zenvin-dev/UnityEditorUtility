929e7af8c945d9ee5d880ea20fb3099fd117e49e
Zenvin <zenvin@outlook.de>
2022-03-21 21:05:42 +0100

    - Add support for relative sibling property finding

--- Editor/Utility.cs
+++ Editor/Utility.cs
@@ -6,20 +6,48 @@ using UnityEditor;
 namespace Zenvin.EditorUtility {
 	public static class Utility {
 
+		private static readonly char[] Separators = new char[] { '/' };
+
 		public static SerializedProperty FindSiblingProperty (this SerializedProperty prop, string name) {
 			if (string.IsNullOrEmpty (name)) {
 				return null;
 			}
 
-			string[] pathParts = prop.propertyPath.Split ('/');
+			string[] pathParts = prop.propertyPath.Split (Separators, System.StringSplitOptions.RemoveEmptyEntries);
 			string path = "";
 			for (int i = 0; i < pathParts.Length - 1; i++) {
 				path = path + pathParts[i];
 			}
-			path = path + "/" + name;
+			
+			pathParts = name.Split (Separators, System.StringSplitOptions.RemoveEmptyEntries);
+			
+			path = path + "/" + pathParts[0];
 			path = path.Trim ('/');
 
-			return prop.serializedObject.FindProperty (path);
+			SerializedProperty current = prop.serializedObject.FindProperty (path);
+			if (current == null) {
+				return null;
+			}
+
+			for (int i = 1; i < pathParts.Length; i++) {
+				string _name = pathParts[i];
+
+				if (current.propertyType == SerializedPropertyType.ObjectReference) {
+					if (current.objectReferenceValue == null) {
+						return null;
+					}
+					SerializedObject obj = new SerializedObject (current.objectReferenceValue);
+					current = obj.FindProperty (pathParts[i]);
+				} else {
+					current = current.FindPropertyRelative (_name);
+				}
+
+				if (current == null) {
+					return null;
+				}
+			}
+
+			return current;
 		}
 
 	}
