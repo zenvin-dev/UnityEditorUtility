cd53b6fefe1761310b6ebee47294c9239e1caba4
Zenvin <zenvin@outlook.de>
2023-01-14 14:24:32 +0100

    - Fix colors

--- Editor/Utility/UnityColors.cs
+++ Editor/Utility/UnityColors.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 
 namespace Zenvin.EditorUtil {
 	public static class UnityColors {
-		public static readonly Color ForegroundColor = new Color (56, 56, 56, 1f);
-		public static readonly Color BackgroundColor = new Color (42, 42, 42, 1f);
+		public static readonly Color ForegroundColor = new Color32 (56, 56, 56, 255);
+		public static readonly Color BackgroundColor = new Color32 (42, 42, 42, 255);
 
-		public static readonly Color AnimatorMajorGridColor = new Color (25, 25, 25, 1f);
-		public static readonly Color AnimatorMinorGridColor = new Color (28, 28, 28, 1f);
+		public static readonly Color AnimatorMajorGridColor = new Color32 (25, 25, 25, 255);
+		public static readonly Color AnimatorMinorGridColor = new Color32 (28, 28, 28, 255);
 
-		public static readonly Color SceneBackgroundColor = new Color (71, 71, 71, 1f);
-		public static readonly Color SceneMajorGridColor = new Color (104, 104, 104, 1f);
-		public static readonly Color SceneMinorGridColor = new Color (90, 90, 90, 1f);
+		public static readonly Color SceneBackgroundColor = new Color32 (71, 71, 71, 255);
+		public static readonly Color SceneMajorGridColor = new Color32 (104, 104, 104, 255);
+		public static readonly Color SceneMinorGridColor = new Color32 (90, 90, 90, 255);
 
-		public static readonly Color SelectionActiveColor = new Color (44, 93, 135, 1f);
-		public static readonly Color SelectionInactiveColor = new Color (77, 77, 77, 1f);
+		public static readonly Color SelectionActiveColor = new Color32 (44, 93, 135, 255);
+		public static readonly Color SelectionInactiveColor = new Color32 (77, 77, 77, 255);
 	}
 }
