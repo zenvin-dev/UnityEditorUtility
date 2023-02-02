6238feb98383b108151714cee66401c45f6c1a71
Zenvin <zenvin@outlook.de>
2023-02-02 22:26:48 +0100

    - Add `UnityColors.SeparatorColor` - Change `UnityColors.AnimatorMinorGridColor` to (35, 35, 35) from (28, 28, 28)

--- Editor/Utility/UnityColors.cs
+++ Editor/Utility/UnityColors.cs
@@ -6,7 +6,7 @@ namespace Zenvin.EditorUtil {
 		public static readonly Color BackgroundColor = new Color32 (42, 42, 42, 255);
 
 		public static readonly Color AnimatorMajorGridColor = new Color32 (25, 25, 25, 255);
-		public static readonly Color AnimatorMinorGridColor = new Color32 (28, 28, 28, 255);
+		public static readonly Color AnimatorMinorGridColor = new Color32 (35, 35, 35, 255);
 
 		public static readonly Color SceneBackgroundColor = new Color32 (71, 71, 71, 255);
 		public static readonly Color SceneMajorGridColor = new Color32 (104, 104, 104, 255);
@@ -14,5 +14,7 @@ namespace Zenvin.EditorUtil {
 
 		public static readonly Color SelectionActiveColor = new Color32 (44, 93, 135, 255);
 		public static readonly Color SelectionInactiveColor = new Color32 (77, 77, 77, 255);
+
+		public static readonly Color SeparatorColor = new Color32 (35, 35, 35, 255);
 	}
 }
