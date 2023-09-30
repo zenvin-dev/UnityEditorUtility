55c74bdaa4776b7c15e5d295eec471916d871284
Zenvin <zenvin@outlook.de>
2023-10-01 00:36:27 +0200

    - Add documentation to `RectExtensions`

--- Editor/Utility/RectExtensions.cs
+++ Editor/Utility/RectExtensions.cs
@@ -3,10 +3,24 @@ using UnityEngine;
 namespace Zenvin {
 	public static class RectExtensions {
 
+		/// <summary>
+		/// Returns the original <paramref name="rect"/>, inset by the given amount.
+		/// </summary>
+		/// <param name="rect"> The original rect that is to be inset. </param>
+		/// <param name="amount"> How much to inset the <paramref name="rect"/> on all edges. </param>
 		public static Rect Inset (this Rect rect, float amount) {
 			return rect.Inset (amount, amount, amount, amount);
 		}
 
+		/// <summary>
+		/// Returns the original <paramref name="rect"/>, inset by the given amount.
+		/// </summary>
+		/// <param name="rect"> The original rect that is to be inset. </param>
+		/// <param name="left"> How much to inset the <paramref name="rect"/> from its left edge. </param>
+		/// <param name="right"> How much to inset the <paramref name="rect"/> from its right edge. </param>
+		/// <param name="top"> How much to inset the <paramref name="rect"/> from its top edge. </param>
+		/// <param name="bottom"> How much to inset the <paramref name="rect"/> from its bottom edge. </param>
+		/// <returns></returns>
 		public static Rect Inset (this Rect rect, float left, float right, float top, float bottom) {
 			rect = new Rect (rect);
 
@@ -18,6 +32,14 @@ namespace Zenvin {
 			return rect;
 		}
 
+		/// <summary>
+		/// Returns the original <paramref name="rect"/>, inset by the given amount.
+		/// </summary>
+		/// <param name="rect"> The original rect that is to be inset. </param>
+		/// <param name="edge"> The edge on which to inset the <paramref name="rect"/>. </param>
+		/// <param name="amount"> How much to inset the <paramref name="rect"/>. </param>
+		/// <param name="inset"> The difference between the original and the inset rect. </param>
+		/// <returns></returns>
 		public static Rect Inset (this Rect rect, RectTransform.Edge edge, float amount, out Rect inset) {
 			switch (edge) {
 				case RectTransform.Edge.Left:
@@ -37,6 +59,10 @@ namespace Zenvin {
 			return rect;
 		}
 
+		/// <summary>
+		/// Returns a <see cref="Rect"/> with the given <paramref name="width"/> and <paramref name="height"/>,
+		/// whose position is centered within the <paramref name="parent"/> rect.
+		/// </summary>
 		public static Rect Centered (this Rect parent, float? width, float? height) {
 			if (!width.HasValue && !height.HasValue) {
 				return parent;
